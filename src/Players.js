61df7af26263d64a3a92c8085d334bee777f0397
pem <pierre-etienne.moreau@loria.fr>
2018-10-19 14:26:28 +0200

    use yellow and purple players first

--- src/Players.js
+++ src/Players.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 import { convertBoardXToDisplayX, convertBoardYToDisplayY } from './common/utils';
 
 const playerNumberToImageName = {
-    0: 'images/piece_blue96.png',
-    1: 'images/piece_yellow96.png',
+    0: 'images/piece_yellow96.png',
+    1: 'images/piece_purple96.png',
     2: 'images/piece_red96.png',
-    3: 'images/piece_purple96.png',
+    3: 'images/piece_blue96.png',
 };
 
 const Players = ({ players }) => (
